2bf0a7637d2259b5e904efe420fa14f310b91d0f
HughG-50 <hugh.greethead@gmail.com>
2020-06-25 16:49:55 +1000

    init

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 import Number from './Number';
 
 class App extends React.Component {
+  
+  state = {
+    inputValue: "",
+    triviaValue: ""
+  }
+
+  async getTriviaValue() {
+    const response = await axios.get(`http://numbersapi.com/${this.state.inputValue}`)
+    const numberTriviaData = response.data
+    this.setState({triviaValue: numberTriviaData})
+  }
+  
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
-        <SearchBar />
-        <Number />
+        <SearchBar 
+          searchValue={this.state.inputValue} 
+          onSearchValueChange={(newValue) => {
+            this.setState({inputValue: newValue})
+          }}
+          onEnter={() => {
+            if(this.state.inputValue){
+              this.getTriviaValue()
+            }
+          }}
+        />
+        <Number 
+          triviaOutput={this.state.triviaValue}
+        />
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  constructor(props){
+    super(props)
+  }
+  
   render() {
     return (
       <div className="ui segment">
         <div className="ui form">
           <div className="field">
             <label>Search a number</label>
-            <input type="text" />
+            <input 
+              type="text" 
+              value={this.props.searchValue}
+              onChange={(event) => {
+                this.props.onSearchValueChange(event.target.value)
+              }}
+              onKeyUp={(event) => {
+                // gives unique code with each code - e.g. code 13 is Enter 
+                if(event.keyCode === 13){
+                  this.props.onEnter()                    
+                }
+              }} 
+            />
           </div>
         </div>
       </div>
